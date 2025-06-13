0cc4d7f911e922a5e5edc0848f062beb05462728
scailancrei <juanantonio.dev@hotmail.com>
2025-06-13 20:25:03 +0200

    delete cypress for conflicts with jest

--- cypress/e2e/Api.cy.tsx
+++
@@ -1,9 +0,0 @@
-describe("Test the Api in differents calls", () => {
-  it("get all the products", () => {
-    cy.request(`https://fakestoreapi.com/products`).then((response) => {
-      const products = response.body
-      expect(products).to.be.an("array")
-      expect(response.status).to.eq(200)
-    })
-  })
-})
