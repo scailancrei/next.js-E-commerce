11f4294cd1980439c0d6ed40afbc26d645207928
scailancrei <juanantonio.dev@hotmail.com>
2026-01-16 15:23:53 +0100

    refactoring some components

--- src/app/_components/checkOutForm.tsx
+++ src/app/_components/checkOutForm.tsx
@@ -28,7 +28,7 @@ export default function CheckoutForm() {
 
   if (!clientSecret) return <p>Loading payments...</p>
   return (
-    <div id="checkout">
+    <div id="checkout" className="bg-linear-30">
       <EmbeddedCheckoutProvider
         stripe={stripePromise}
         options={{ clientSecret }}
--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,7 +2,7 @@
 import React, { useContext, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { ProductsCartContext } from "@/context/useProductsCartContext"
-import { CartTotalPriceContext } from "@/context/useCartTotalPriceContext"
+
 import CheckoutForm from "@/_components/checkOutForm"
 
 /**
@@ -16,7 +16,6 @@ import CheckoutForm from "@/_components/checkOutForm"
 
 export default function CheckoutPage(): React.ReactElement {
   const { productsCart } = useContext(ProductsCartContext)
-  const { cartTotalPrice } = useContext(CartTotalPriceContext)
 
   const router = useRouter()
 
@@ -30,8 +29,10 @@ export default function CheckoutPage(): React.ReactElement {
     return <div>Redirecting to main page...</div>
   }
   return (
-    <div className="flex justify-center items-center h-screen">
-      <CheckoutForm />
+    <div className="flex justify-center items-center">
+      <div className="w-1/2 mt-30 mb-30 p-6 bg-white rounded-lg shadow-lg">
+        <CheckoutForm />
+      </div>
     </div>
   )
 }
